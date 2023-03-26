8db63f2170641311dc8c2ef449c677d52d2a05c9
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-26 00:05:28 -0400

    Added comments

--- 1_week/friday_challenges/sumTotal.js
+++ 1_week/friday_challenges/sumTotal.js
@@ -1,14 +1,21 @@
 //By M. Sandoval
 
+// Define an array of integers called "sumInt"
 const sumInt = [6,1,643,234,123,4,7,9];
-let sum = 0
 
+// Initialize a variable called "sum" to 0
+let sum = 0;
+
+// Define a function called "getSum" that calculates the sum of the elements in "sumInt" and logs the result
 const getSum = () => {
+    // Use a "for...of" loop to iterate over the elements in "sumInt"
     for(let element of sumInt){
-        sum += element
+        // Add the value of the current element to "sum"
+        sum += element;
     }
-    console.log(sum)
+    // Log the value of "sum"
+    console.log(sum);
 }
 
-getSum()
-
+// Call the "getSum" function
+getSum();
