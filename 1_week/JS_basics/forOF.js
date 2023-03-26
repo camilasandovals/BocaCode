c502088b1ff6a3d18a8749462b89ce218c3747e6
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-25 23:55:10 -0400

    Added comments

--- 1_week/JS_basics/forOF.js
+++ 1_week/JS_basics/forOF.js
@@ -1,11 +1,14 @@
 //By M. Sandoval
 
+// Initiate string variable with it's value
 var students = ["Maria", "Sergio", "Rosa"]
 
+// Create function
 function callStudent (student) {
     console.log(`Hi ${student}`)
 }
 
+//Invoke function
 for(let student of students){
     callStudent(student)
 }
--- 1_week/JS_basics/function.js
+++ 1_week/JS_basics/function.js
@@ -1,8 +1,11 @@
 //By M. Sandoval
 
+// Initiate string variable with it's value
 const myName = 'camila'
 
+// Create function
 const getName = () =>
     console.log(myName)
 
+//Invoke function
 getName()
