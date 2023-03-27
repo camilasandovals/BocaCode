928f5d8f9d990a777ba96f679e284eff57c23b49
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-27 10:45:03 -0400

    code for destructuring an object and use the property

---
+++ 2_week/JS_basics/destructuring.js
@@ -0,0 +1,12 @@
+//By M. Sandoval
+
+let car = {
+    make: 'Audi',
+    model: 'A3',
+    year: 2018,
+    color: 'gray'
+}
+
+const { make, model, year, color } = car  //destructuring 
+
+console.log(make)
