ef91a388bf3315940e64b8d555e352a0d8dd809b
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-26 22:42:34 -0400

    Implementing filter/map/find/foreach/sum on array objects

--- 1_week/JS_objects/array_objects_methods.js
+++ 1_week/JS_objects/array_objects_methods.js
@@ -1,5 +1,6 @@
 //By M. Sandoval
 
+// Define an array of objects representing articles, each with a name and a price
 const article = [
     {articleName: 'book', price: 30},
     {articleName: 'tv', price: 1400},
@@ -7,35 +8,35 @@ const article = [
     {articleName: 'notebook', price: 10}
 ]
 
-// Filter
+// Filter: Filter the articles to find only those with a price less than or equal to 100
 const filteredArticles = article.filter(function(article){
     return article.price <=100
 });
 
 console.log(filteredArticles)
 
-//Map
+//Map: Map the articles to create an array of article names
 const mapedArticles = article.map(function(article){
     return article.articleName
 });
 
 console.log(mapedArticles)
 
-//Find
+//Find: Find the article with the name "tv"
 const findArticles = article.find(function(article){
     return article.articleName === 'tv'
 });
 
 console.log(findArticles)
 
-//For Each
+//For Each: Use forEach() to loop through each article in the array
 const forEachArticle = article.forEach(function(article){
     return article.articleName
 });
 console.log(forEachArticle)
 
 
-//Sum (returns true or false)
+//Sum (returns true or false) to determine if any articles have a price less than or equal to 800
 const cheapArticle = article.some(function(article){
     return article.price <= 800;
 });
