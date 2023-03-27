9a8cedb782c13e77a9c837573671e2183b4a959f
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-27 09:35:24 -0400

    Adding arrays and objects into objects

---
+++ 1_week/JS_objects/complex_objects.js
@@ -0,0 +1,21 @@
+
+//By M. Sandoval
+
+//This code explains how to add arrays and objects into objects
+
+const person =  {
+    firstName : 'maria',
+    lastName : 'sandoval',
+    age : 26,
+    married : true,
+    parents: ['Jairo', 'Sandra'],
+    partner: {
+        firstName: 'Sergio',
+        lastName: 'Colmenares',
+        age: 30,
+        siblings: ['Vanessa', 'Lola', 'Lulu']
+
+    }
+}
+
+console.log(person.partner)
