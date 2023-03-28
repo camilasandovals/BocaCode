cfd7df9323007e5d790c0e9b5ba09d7cd837216c
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-27 21:37:24 -0400

    first item of homework- destructuring

---
+++ 2_week/homework/dayOne.js
@@ -0,0 +1,9 @@
+//By M. Sandoval 
+
+//use destructuring to access the color property
+let myCar = {brand: "Toyota", make: "Prius", color: "red"}
+
+const {brand, make, color} = myCar
+
+console.log(brand)
+
