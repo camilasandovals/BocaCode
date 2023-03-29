9319c54619be6bd1b69d1179a44bf6af96dd1d08
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-28 21:10:28 -0400

    first task homework week2 day2

---
+++ 2_week/homework/dayTwo.js
@@ -0,0 +1,20 @@
+//By M. Sandoval
+
+/*1. Create a function that takes in a number of two-pointers 
+(shots made that count as 2 points each) and three-pointers 
+(shots made that count as 3 points each) made and returns a 
+basketball team's total score. Invoke the function and console.
+log the results.*/
+
+const getTotalScore = function (twoPointers, threePointers) {
+    return twoPointers*2 + threePointers*3
+}
+
+console.log(getTotalScore(5,2))
+
+/* 2. Create a new file petstore.sql. Create a pet store database, with 2 mySQL tables, pets and owners. Each table should have at least 3 properties. Insert one pet and one owner into appropriate tables.*/
+//check folder "PetstoreSQL" inside 2_week/homework
+
+//3. Run a query in your terminal to print all of the data in the pets table.
+//check folder "PetstoreSQL" inside 2_week/homework
+
