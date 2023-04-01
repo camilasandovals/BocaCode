8ed58dafb5e3c3fd2063c42bdc35e2d80d9b3de8
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-31 20:18:54 -0400

    added code for multiplication table

--- 1_week/friday_challenges/multiplicationTable.js
+++ 1_week/friday_challenges/multiplicationTable.js
@@ -1,14 +1,13 @@
 //By M. Sandoval
 
 const num = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let newarray = []
 
-num = num.push(" ")
-console.log(num)
-// for(let i=0; i<num.length; i++){
-//     console.log(num[i])
-//     for (let j=0; j<num.length; j++ ){
-//         newarray = newarray.push(num[i]*num[j])
-//     }
-//     console.log(newarray)
-// }
+console.log(`   ${num[0]}  ${num[1]}  ${num[2]}  ${num[3]}  ${num[4]}  ${num[5]}  ${num[6]}  ${num[7]}  ${num[8]}  ${num[9]}`)
+for (let element of num){
+    const multiplication = num.map(x => x * element);
+    console.log(`${element}  ${multiplication[0]}  ${multiplication[1]}  ${multiplication[2]}  ${multiplication[3]}  ${multiplication[4]}  ${multiplication[5]}  ${multiplication[6]}  ${multiplication[7]}  ${multiplication[8]}  ${multiplication[9]}`)
+}
+
+
+
+
