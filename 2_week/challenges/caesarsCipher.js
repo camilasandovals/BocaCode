36f9e2bcd498c884688fee44d2008a3900962a8e
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-03 20:05:21 -0400

    added challenge from edibit

--- 2_week/challenges/caesarsCipher.js
+++ 2_week/challenges/caesarsCipher.js
@@ -4,23 +4,35 @@
 
 //Create a function that takes a string s (text to be encrypted) and an integer k (the rotation factor). It should return an encrypted string.
 
-//const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 let newWord = ""
 
-const caesarCipher = (s , k, alphabet) => {
+const caesarCipher = (s , k) => {
     for(let element of s){
         if (element == '-'){
             newWord += element
+        } else {
+        let index = alphabet.indexOf(element.toLowerCase())
+        if(index + k > (alphabet.length -1)){
+            newWord += alphabet[k-1]
         }
         else{
-            newWord += alphabet[alphabet.indexOf(element) + k]
+            newWord += alphabet[index + k]
         }
+    }
     }
     console.log(newWord)
 }
 
-caesarCipher("middle-Outz", 2, alphabet) // "okffng-Qwvb"
+caesarCipher("middle-Outz", 2) // "okffng-Qwvb"
+
+// function caesarCipher(s, k) {
+//     return s.replace(/[a-z]/gi, m => {
+//       const c = m < "a" ? 65 : 97;
+//       return String.fromCharCode((m.charCodeAt(0) - c + k) % 26 + c)
+//     });
+//   }
+
 
 // newWord += alphabet[alphabet.findIndex(
 //     (alphabet, element) => {
