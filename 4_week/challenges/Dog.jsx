8233b9f27c39132d2eadf78181564f35ea7534b0
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-17 09:23:55 -0400

    added dog component

---
+++ 4_week/challenges/Dog.jsx
@@ -0,0 +1,10 @@
+export default function Dog(){
+    return (
+        <section>
+                <h1> Dog Name is Oscar </h1>
+                <p> Is a beautiful and a very unique dog</p>
+        </section>
+
+    )
+}
+
