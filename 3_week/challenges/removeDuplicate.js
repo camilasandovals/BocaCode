1f891950a38b73a52127bc76e289d9c030b176c9
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-16 22:36:06 -0400

    added function to remove duplicates in an array

--- 3_week/challenges/removeDuplicate.js
+++ 3_week/challenges/removeDuplicate.js
@@ -4,4 +4,21 @@ function removeDuplicate(arr) {
     return console.log(arr)
 }
 
+function removeDuplicate2(arr) {
+    let newArray = []
+    for (let i = 0; i < arr.length; i ++) {
+        let exists = false
+        for ( let j=i+1; j<arr.length; j++){
+            if (arr[i] == arr[j]) {
+                exists = true
+            }
+        }
+        if (exists == false) {
+            newArray.push(arr[i])
+        } 
+    }
+    return console.log(newArray)
+}
+
 removeDuplicate([1,2,3,4,5,6,6,6,3,7,8,9,10,10,10])
+removeDuplicate2([1,2,3,4,5,6,6,6,3,7,8,9,10,10,10])
