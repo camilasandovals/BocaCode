aa2d182e507cd04774f4815739104c3a7c5cdab7
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-27 23:55:33 -0400

    added objects

---
+++ 6_week/objects.js
@@ -0,0 +1,12 @@
+const quotes = [
+    { saying: "You have power over your mind - not outside events. Realize this, and you will find strength.", author: "Marcus Aurelius"},
+    { saying: "Evil Never Dies.", author: "Meghann Stamper"},
+    { saying: "Company is like fish, it starts to stink after 3 days.", author: "Benjamin Franklin"},
+  ]
+
+
+//  const quote = { saying: "You have power over your mind - not outside events. Realize this, and you will find strength.", author: "Marcus Aurelius"}
+//   console.log(quote.saying)
+//   console.log(quote['saying'])
+
+console.log(quotes[1].saying)
